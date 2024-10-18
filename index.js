cd4961a7eace7250b41e41121117b7d100717cf6
Rituraj Pathak <riturajpathak1998@gmail.com>
2024-10-19 00:42:52 +0530

    vercel json added

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'https://instant-cv-backend.vercel.app/',
+  origin: 'http://localhost:5173',
   credentials: true,
 }));
 app.use(express.json())
