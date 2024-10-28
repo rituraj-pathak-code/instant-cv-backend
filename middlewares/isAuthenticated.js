e8fc14bba21099655301d2f8b376a217f5e5c33b
Rituraj Pathak <riturajpathak1998@gmail.com>
2024-10-28 22:44:27 +0530

    Some bug fixes

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -5,6 +5,7 @@ import jwt from 'jsonwebtoken'
 export const isAuthenticated = async (req, res, next) => {
     try{
       const {token} = req.cookies;
+      console.log("Token", token)
       if (!token) {
        return res.status(401).send("Invalid Token");
       }
